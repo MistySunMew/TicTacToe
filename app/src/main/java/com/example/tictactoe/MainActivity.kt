3ae3cf6758b1757df7de3c35be0d9ef4cc20fcbf
MistySunMew <Michael.e.forbes@comcast.com>
2023-05-18 10:54:22 -0700

    Finished App

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -7,57 +7,67 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-
+    private var numTurns = 0
+    private var currPlayer = "x"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val gameText = findViewById<TextView>(R.id.textView)
-        var currPlayer = "x"
+
+
 
         val btnA1 = findViewById<Button>(R.id.btnA1)
-        btnA1.setOnClickListener() {
-            buttonPress(btnA1, gameText, currPlayer)
-            currPlayer = if (currPlayer == "x") "o" else "x"
-        }
         val btnA2 = findViewById<Button>(R.id.btnA2)
-        btnA2.setOnClickListener() {
-            buttonPress(btnA2, gameText, currPlayer)
-            currPlayer = if (currPlayer == "x") "o" else "x"
-        }
         val btnA3 = findViewById<Button>(R.id.btnA3)
-        btnA3.setOnClickListener() {
-            buttonPress(btnA3, gameText, currPlayer)
-            currPlayer = if (currPlayer == "x") "o" else "x"
-        }
-         val btnB1 = findViewById<Button>(R.id.btnB1)
-        btnB1.setOnClickListener() {
-            buttonPress(btnB1, gameText, currPlayer)
-            currPlayer = if (currPlayer == "x") "o" else "x"
-        }
+        val btnB1 = findViewById<Button>(R.id.btnB1)
         val btnB2 = findViewById<Button>(R.id.btnB2)
-        btnB2.setOnClickListener() {
-            buttonPress(btnB2, gameText, currPlayer)
-            currPlayer = if (currPlayer == "x") "o" else "x"
-        }
         val btnB3 = findViewById<Button>(R.id.btnB3)
-        btnB3.setOnClickListener() {
-            buttonPress(btnB3, gameText, currPlayer)
-            currPlayer = if (currPlayer == "x") "o" else "x"
-        }
         val btnC1 = findViewById<Button>(R.id.btnC1)
-        btnC1.setOnClickListener() {
-            buttonPress(btnC1, gameText, currPlayer)
-            currPlayer = if (currPlayer == "x") "o" else "x"
-        }
         val btnC2 = findViewById<Button>(R.id.btnC2)
-        btnC2.setOnClickListener() {
-            buttonPress(btnC2, gameText, currPlayer)
-            currPlayer = if (currPlayer == "x") "o" else "x"
-        }
         val btnC3 = findViewById<Button>(R.id.btnC3)
+        btnA1.setOnClickListener() {
+            buttonPress(btnA1, gameText)
+            checkWin(gameText, btnA1, btnA2, btnA3, btnB1, btnB2, btnB3, btnC1, btnC2, btnC3)
+        }
+
+        btnA2.setOnClickListener() {
+            buttonPress(btnA2, gameText)
+            checkWin(gameText, btnA1, btnA2, btnA3, btnB1, btnB2, btnB3, btnC1, btnC2, btnC3)
+        }
+
+        btnA3.setOnClickListener() {
+            buttonPress(btnA3, gameText)
+            checkWin(gameText, btnA1, btnA2, btnA3, btnB1, btnB2, btnB3, btnC1, btnC2, btnC3)
+        }
+
+        btnB1.setOnClickListener() {
+            buttonPress(btnB1, gameText)
+            checkWin(gameText, btnA1, btnA2, btnA3, btnB1, btnB2, btnB3, btnC1, btnC2, btnC3)
+        }
+
+        btnB2.setOnClickListener() {
+            buttonPress(btnB2, gameText)
+            checkWin(gameText, btnA1, btnA2, btnA3, btnB1, btnB2, btnB3, btnC1, btnC2, btnC3)
+        }
+
+        btnB3.setOnClickListener() {
+            buttonPress(btnB3, gameText)
+            checkWin(gameText, btnA1, btnA2, btnA3, btnB1, btnB2, btnB3, btnC1, btnC2, btnC3)
+        }
+
+        btnC1.setOnClickListener() {
+            buttonPress(btnC1, gameText)
+            checkWin(gameText, btnA1, btnA2, btnA3, btnB1, btnB2, btnB3, btnC1, btnC2, btnC3)
+        }
+
+        btnC2.setOnClickListener() {
+            buttonPress(btnC2, gameText)
+            checkWin(gameText, btnA1, btnA2, btnA3, btnB1, btnB2, btnB3, btnC1, btnC2, btnC3)
+        }
+
         btnC3.setOnClickListener() {
-            buttonPress(btnC3, gameText, currPlayer)
-            currPlayer = if (currPlayer == "x") "o" else "x"
+            buttonPress(btnC3, gameText)
+            checkWin(gameText, btnA1, btnA2, btnA3, btnB1, btnB2, btnB3, btnC1, btnC2, btnC3)
         }
 
 
@@ -76,11 +86,13 @@ class MainActivity : AppCompatActivity() {
             btnC3.text = ""
             gameText.text = "Player X's turn"
             currPlayer = "x"
+            numTurns = 0
         }
     }
 
-    private fun buttonPress(btn: Button, gameText: TextView, currPlayer: String) {
+    private fun buttonPress(btn: Button, gameText: TextView,) {
         if (btn.text == "") {
+            numTurns++
             if (currPlayer == "x") {
                 btn.text = "x"
                 gameText.text = "Player O's turn"
@@ -88,6 +100,39 @@ class MainActivity : AppCompatActivity() {
                 btn.text = "o"
                 gameText.text = "Player X's turn"
             }
+            currPlayer = if (currPlayer == "x") "o" else "x"
         }
+        if (numTurns == 9) {
+            gameText.text = "It's a tie!"
+        }
+    }
+
+    private fun checkWin(gameText: TextView, btnA1: Button, btnA2: Button, btnA3: Button, btnB1: Button, btnB2: Button, btnB3: Button, btnC1: Button, btnC2: Button, btnC3: Button) {
+        currPlayer = if (currPlayer == "x") "o" else "x"
+        if (btnA1.text == currPlayer && btnA2.text == currPlayer && btnA3.text == currPlayer) {
+            gameText.text = "Player ${currPlayer.uppercase()} wins!"
+        }
+        else if (btnB1.text == currPlayer && btnB2.text == currPlayer && btnB3.text == currPlayer) {
+            gameText.text = "Player ${currPlayer.uppercase()} wins!"
+        }
+        else if (btnC1.text == currPlayer && btnC2.text == currPlayer && btnC3.text == currPlayer) {
+            gameText.text = "Player ${currPlayer.uppercase()} wins!"
+        }
+        else if (btnA1.text == currPlayer && btnB1.text == currPlayer && btnC1.text == currPlayer) {
+            gameText.text = "Player ${currPlayer.uppercase()} wins!"
+        }
+        else if (btnA2.text == currPlayer && btnB2.text == currPlayer && btnC2.text == currPlayer) {
+            gameText.text = "Player ${currPlayer.uppercase()} wins!"
+        }
+        else if (btnA3.text == currPlayer && btnB3.text == currPlayer && btnC3.text == currPlayer) {
+            gameText.text = "Player ${currPlayer.uppercase()} wins!"
+        }
+        else if (btnA1.text == currPlayer && btnB2.text == currPlayer && btnC3.text == currPlayer) {
+            gameText.text = "Player ${currPlayer.uppercase()} wins!"
+        }
+        else if (btnA3.text == currPlayer && btnB2.text == currPlayer && btnC1.text == currPlayer) {
+            gameText.text = "Player ${currPlayer.uppercase()} wins!"
+        }
+        currPlayer = if (currPlayer == "x") "o" else "x"
     }
 }
